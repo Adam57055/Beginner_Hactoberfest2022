870db195b0952919bc8fc28da197d6d04262b9b7
Adam57055 <adamtwam85@gmail.com>
2026-03-11 11:49:01 -0400

    Refactor age calculation and user input handling

--- AgeCalculator.py
+++ AgeCalculator.py
@@ -1,10 +1,14 @@
+#Imports time utilities
 import datetime
 from datetime import date
+#Age is calculated through current year subtracted from the birth year
 def Cal_Age(bday):
 	n= date.today()
-	a= n.year - bday.year
-	return a
-b=input("Enter your BirthDate as yyyy/mm/dd:")
+	age= n.year - bday.year
+	return age
+#Asks user for their birthdate
+birthday=input("Enter your BirthDate as yyyy/mm/dd:")
 n="%Y/%m/%d"
-bday=datetime.datetime.strptime(b,n)
+#Executes function
+bday=datetime.datetime.strptime(birthday,n)
 print("You are",Cal_Age(bday), "years")
