d27ee827bc32270639ca514306b0035d3661af3a
Adam57055 <adamtwam85@gmail.com>
2026-03-11 11:55:17 -0400

    Enhance age validation and comments in AGE.py
    Added input validation for negative age and improved comments.

--- AGE.py
+++ AGE.py
@@ -1,7 +1,12 @@
 # Write a program to check whether a person is elector or not
-age=(int( input("Enter the age of the person :")))
+age= int(input("Enter the age of the person :"))
+#If user's age is above 18, they are eligible
 if age>=18:
     print("The elector is VALID")
+#If user's age is below 18, they are ineligible
 else:
     print("NOT VALID AGE")
-    
+#Program urges user to reprompt a sufficient reponse if age inputted is negative
+while age < 0:
+    print("Invalid response. Please try again.")
+    age = int(input("Enter the age of the person: "))
